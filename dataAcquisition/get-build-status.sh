e60858dd957507ae635d1ebaf1ce3e31737340f3
Leonardo Graboski Veiga <leogveiga@gmail.com>
2024-03-01 10:44:47 -0300

    data: add demo mode with mock data
    Generate mock data to populate the dashboards. This means data for the
    past 30 days is generated on startup, and then data is generated every
    minute.
    
    Closes #2
    
    Signed-off-by: Leonardo Graboski Veiga <leogveiga@gmail.com>

--- dataAcquisition/get-build-status.sh
+++ dataAcquisition/get-build-status.sh
@@ -17,14 +17,24 @@ INFLUX_BUCKET_NAME="jenkinsdatabucket"
 INFLUX_BUCKET_RETENTION="180d"
 CURL_MAX_RETRIES=30
 
+# Initialization checks
 if [[ -n "$DEBUG" ]]; then
-    POLL_INTERVAL_SEC=60
+    POLL_INTERVAL_SEC_DEBUG=60
+    POLL_INTERVAL_SEC=${POLL_INTERVAL_SEC_DEBUG}
     echo "Debug mode enabled"
 else
     POLL_INTERVAL_SEC=3600
     echo "Debug mode disabled"
 fi
 echo "Polling interval set to $POLL_INTERVAL_SEC seconds"
+
+if [[ -n "$DEMO" ]]; then
+    echo "Demo mode enabled"
+    echo "Generating fake data at every poll interval instead of polling the server"
+    FAKEDATECOUNT=30
+else
+    echo "Demo mode disabled"
+fi
 
 # Trap Ctrl+C and clean up before exiting
 function cleanup() {
@@ -43,6 +53,47 @@ function create_update_bucket() {
     else
         influx bucket update --id "$influx_bucket_id" --retention $INFLUX_BUCKET_RETENTION
     fi
+}
+
+function gen_fake_data() {
+    FAKES=(
+        "Aborted"
+        "Failed"
+        "In progress"
+        "Not built"
+        "Success"
+        "Not available"
+        "Unstable"
+    )
+    # Get data points in a line protocol format
+    # https://docs.influxdata.com/influxdb/v2/get-started/write/?t=influx+CLI#line-protocol
+    local buildstatus="$INFLUX_MEASUREMENT_NAME "
+
+    for image in "${IMAGE_TYPE[@]}"; do
+        for job in "${JOB_LIST[@]}"; do
+            RANDOM=$$$(</dev/urandom tr -dc 0-9 | dd bs=5 count=1 2>/dev/null)
+            jobstatus=${FAKES[ $RANDOM % ${#FAKES[@]} ]}
+            buildstatus="${buildstatus}${image}-${job}=\"${jobstatus}\","
+            echo "Element: $job     | Value: $jobstatus"
+        done
+    done
+
+    # Remove trailing comma
+    buildstatus=${buildstatus::-1}
+
+    # Add timestamp during the first iterations
+    if [ "$FAKEDATECOUNT" -ne "0" ]; then
+        buildstatus="${buildstatus} $(date -d "$FAKEDATECOUNT day ago" +%s)"
+        ((FAKEDATECOUNT--))
+        POLL_INTERVAL_SEC=0.1
+    else
+        buildstatus="${buildstatus} $(date +%s)"
+        POLL_INTERVAL_SEC=${POLL_INTERVAL_SEC_DEBUG}
+    fi
+
+    # Write data into DB
+    echo "Line protocol data: $buildstatus"
+    influx write --bucket $INFLUX_BUCKET_NAME --precision s "$buildstatus"
 }
 
 function insert_into_influxdb() {
@@ -91,8 +142,13 @@ echo "InfluxDB ready!"
 create_update_bucket
 
 while true; do
-    # Get data points into database
-    insert_into_influxdb
+
+    if [[ -n "$DEMO" ]]; then
+        gen_fake_data
+    else
+        # Get real data points into database
+        insert_into_influxdb
+    fi
 
     # Wait POLL_INTERVAL_SEC until the next data acquisition
     sleep $POLL_INTERVAL_SEC
